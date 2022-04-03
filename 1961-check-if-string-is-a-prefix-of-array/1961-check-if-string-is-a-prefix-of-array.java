452ce2822abe6408e82897b4afb2a01b587cffb6
harjeevs17 <38102297+harjeevs17@users.noreply.github.com>
2022-04-03 17:59:10 +0530

    Time: 8 ms (5.15%), Space: 44.9 MB (6.17%) - LeetHub

--- 1961-check-if-string-is-a-prefix-of-array/1961-check-if-string-is-a-prefix-of-array.java
+++ 1961-check-if-string-is-a-prefix-of-array/1961-check-if-string-is-a-prefix-of-array.java
@@ -4,14 +4,11 @@ class Solution {
         for(int i=0;i<words.length;i++){
             StringBuilder sa = new StringBuilder();
             for(int j=0;j<=i;j++){
-                System.out.println(words[j]);
                 sa.append(words[j]);
             }
             if(sa.toString().equals(s1)){
                 return true;
             }
-            System.out.println(sa.toString());
-            System.out.println("*****");
         }
         return false;
     }
