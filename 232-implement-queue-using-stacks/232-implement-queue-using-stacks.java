7a8371d423f92fcf65563494efc66199b630166a
harjeevs17 <38102297+harjeevs17@users.noreply.github.com>
2022-04-10 20:38:42 +0530

    Time: 1 ms (60.54%), Space: 41.4 MB (76.99%) - LeetHub

--- 232-implement-queue-using-stacks/232-implement-queue-using-stacks.java
+++ 232-implement-queue-using-stacks/232-implement-queue-using-stacks.java
@@ -21,22 +21,18 @@ class MyQueue {
                 a1.push(a2.pop());
             }
         }    
-        
         return pop;
     }
     
     public int peek() {
-       
         int peek = -1;
         while(!a1.empty()){
-             System.out.println("rt");
             a2.push(a1.pop());
         }
         if(!a2.empty()){
             peek = a2.peek();    
         }
         if(!a2.empty()){
-            System.out.println("df");
             while(!a2.empty()){
                 a1.push(a2.pop());
             }
@@ -45,9 +41,6 @@ class MyQueue {
     }
     
     public boolean empty() {
-        
-
-
         return a1.empty();
     }
 }
