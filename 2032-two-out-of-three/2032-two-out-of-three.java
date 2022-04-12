c692908cf4ad89b8fc2276a87d57d57c86895604
harjeevs17 <38102297+harjeevs17@users.noreply.github.com>
2022-04-13 00:44:38 +0530

    Time: 13 ms (22.06%), Space: 48.5 MB (9.58%) - LeetHub

--- 2032-two-out-of-three/2032-two-out-of-three.java
+++ 2032-two-out-of-three/2032-two-out-of-three.java
@@ -1,48 +1,30 @@
 class Solution {
     public List<Integer> twoOutOfThree(int[] nums1, int[] nums2, int[] nums3) {
-        Map<Integer,Integer> data = new HashMap<>();
-        Map<Integer,Integer> aa = new HashMap<>();
-        Map<Integer,Integer> bb = new HashMap<>();
-        Map<Integer,Integer> cc = new HashMap<>(); 
-        List<Integer> dd = new ArrayList<>();
-        for(int i=0;i<nums1.length;i++){
-            aa.put(nums1[i],1);
-        }
-        for (Map.Entry<Integer, Integer> entry : aa.entrySet()) {
-            if(!data.containsKey(entry.getKey())){
-                data.put(entry.getKey(),1);
+        	Set<Integer> set1 = new HashSet<>();
+            Set<Integer> set2 = new HashSet<>();
+            Set<Integer> set3 = new HashSet<>(); 
+            Set<Integer> set = new HashSet<>();
+            List<Integer> res = new ArrayList<>();
+            for(int i=0;i<nums1.length;i++){
+                set1.add(nums1[i]);
+                set.add(nums1[i]);
             }
-        }
-       
-        for(int i=0;i<nums2.length;i++){
-            bb.put(nums2[i],1);
-        }
-         
-         for (Map.Entry<Integer, Integer> entry : bb.entrySet()) {
-            if(!data.containsKey(entry.getKey())){
-                data.put(entry.getKey(),1);
-            }else{
-                 data.put(entry.getKey(),data.get(entry.getKey())+1);
+            for(int i=0;i<nums2.length;i++){
+                set2.add(nums2[i]);
+                set.add(nums2[i]);
             }
-        }
-        for(int i=0;i<nums3.length;i++){
-            cc.put(nums3[i],1);
-        }
-         for (Map.Entry<Integer, Integer> entry : cc.entrySet()) {
-            if(!data.containsKey(entry.getKey())){
-                data.put(entry.getKey(),1);
-            }else{
-                 data.put(entry.getKey(),data.get(entry.getKey())+1);
+            for(int i=0;i<nums3.length;i++){
+                set3.add(nums3[i]);
+                set.add(nums3[i]);
             }
-        }
+            for(Integer d:set){
+                if((set1.contains(d)&&set2.contains(d))||(set2.contains(d)&&set3.contains(d))||(set1.contains(d)&&set3.contains(d))){
+                    res.add(d);
+                }
+            }
+            return res;
         
-        System.out.println(Arrays.toString(data.entrySet().toArray()));
-        for (Map.Entry<Integer, Integer> entry : data.entrySet()) {
-            System.out.println(entry.getKey()+" : "+entry.getValue());
-            if(entry.getValue()>=2){
-                dd.add(entry.getKey());
-            }
-        }   
-        return dd;
+
+        
     }
 }
