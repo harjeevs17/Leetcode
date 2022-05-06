0ab441021a4846aafff5efb8ceb52a003a003cfa
harjeevs17 <38102297+harjeevs17@users.noreply.github.com>
2022-05-06 22:39:44 +0530

    Time: 2484 ms (5.04%), Space: 48.5 MB (7.20%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.java
@@ -7,21 +7,15 @@ class Solution {
 				set.add(arr[i]);
 			}
 		}
-        int g=0;
-		for(int i=0;i<arr.length;i++){
-            int flag = 998;
+        for(int i=0;i<arr.length;i++){
             for(int j=0;j<arr.length;j++){
                 if(j+2<arr.length){
                     if(arr[j]==arr[j+1]){
                         arr[j+1] = arr[j+2];
-                        flag =1;
                     }
                  }
              }
          }
-       
-        
-       System.out.println(g);
-		return set.size();
+        return set.size();
     }
 }    
